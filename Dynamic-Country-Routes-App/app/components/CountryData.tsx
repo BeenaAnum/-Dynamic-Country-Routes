14d1d54c66ef8811155016ada33c9f29078aff4c
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-10-24 04:01:58 +0500

    Update CountryData.tsx

--- Dynamic-Country-Routes-App/app/components/CountryData.tsx
+++ Dynamic-Country-Routes-App/app/components/CountryData.tsx
@@ -6,9 +6,9 @@ const getCountries = () => {
     return['pakistan','canada','france','brazil','india','japan','nigeria','australia','uk','sa',];
 };
  return(
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <ul className="justify-center items-center font-bold text-2xl mb-2 md:text-3xl">
         {getCountries().map((country) =>(
-         <li className="bg-black p-2 rounded hover:text-violet-600" key={country}>
+         <li className="bg-black p-2 m-2 rounded hover:text-violet-600" key={country}>
          <Link href={`/country/${country}`}>
          {country.charAt(0).toUpperCase() + country.slice(1)}
          </Link>
